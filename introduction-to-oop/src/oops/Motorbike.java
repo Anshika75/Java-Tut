a8f961eb875be5ae66a3e2c44bb8dbf87bfd6578
Demi Lune <anshikathakur596@gmail.com>
2024-11-06 15:47:57 +0530

    new project done

--- introduction-to-oop/src/oops/Motorbike.java
+++ introduction-to-oop/src/oops/Motorbike.java
@@ -1,5 +1,60 @@
 package oops;
 
 public class Motorbike {
+    private int speed;
+    private String name;
 
+    // Constructor to set the name
+    public Motorbike(String name) {
+        this.name = name;
+    }
+
+    void start() {
+        System.out.println(name + " started");
+    }
+
+    void setSpeed(int speed) {
+        if (speed >= 0 && speed <= 100) {
+            this.speed = speed;
+            System.out.println(name + " speed set to: " + this.speed + "km/hr");
+        } else if (speed < 0) {
+            this.speed = 0;
+            System.out.println(name + " speed can't be negative. Auto set to 0 km/hr");
+        } else {
+            this.speed = 100;
+            System.out.println(name + " speed can't be more than 100 km/hr. Auto set to 100 km/hr");
+        }
+    }
+
+    void accelerate() {
+        accelerate(10);
+    }
+
+    void decelerate() {
+        decelerate(10);
+    }
+
+    // Customized functions
+
+    void accelerate(int increment) {
+        if ((this.speed + increment) < 100) {
+            this.speed += increment;
+            System.out.println(name + " speed increased to: " + this.speed + "km/hr");
+        } else {
+            System.out.println(name + " speed limit reached: " + this.speed + "km/hr. Can't be increased by: " + increment + "km/hr");
+        }
+    }
+
+    void decelerate(int decrement) {
+        if ((this.speed - decrement) > 0) {
+            this.speed -= decrement;
+            System.out.println(name + " speed decreased to: " + this.speed + "km/hr");
+        } else {
+            System.out.println(name + " speed limit reached: " + this.speed + "km/hr. Can't be decreased by: " + decrement + "km/hr");
+        }
+    }
+
+    int getSpeed() {
+        return this.speed;
+    }
 }
--- introduction-to-oop/src/oops/MotorbikeRunner.java
+++ introduction-to-oop/src/oops/MotorbikeRunner.java
@@ -1,5 +1,26 @@
 package oops;
 
 public class MotorbikeRunner {
+    public static void main(String[] args) {
+        Motorbike honda = new Motorbike("Honda");
+        Motorbike ducati = new Motorbike("Ducati");
 
+        honda.start();
+        honda.setSpeed(130);
+        honda.accelerate();
+        honda.decelerate();
+        honda.accelerate(50);
+        honda.decelerate(50);
+        System.out.println("Honda speed: " + honda.getSpeed() + " km/hr");p
+
+        System.out.println();
+
+        ducati.start();
+        ducati.setSpeed(0);        
+        ducati.accelerate();
+        ducati.decelerate();
+        ducati.accelerate(50);        
+        ducati.decelerate(60);
+        System.out.println("Ducati speed: " + ducati.getSpeed() + " km/hr");
+    }
 }
