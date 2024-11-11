3bef0ade22db8a208f55c9d4d06e7e4f5d0a671d
Demi Lune <anshikathakur596@gmail.com>
2024-11-11 11:14:11 +0530

    for and while loops done

---
+++ loops/src/forexamples/WhileNumberPlayerRunner.java
@@ -0,0 +1,9 @@
+package forexamples;
+
+public class WhileNumberPlayerRunner {
+    public static void main(String[] args) {
+        WhileNumberPlayer player = new WhileNumberPlayer(30);
+        player.printSquaresUptoLimit();
+        player.printCubesUptoLimit();
+    }
+}
