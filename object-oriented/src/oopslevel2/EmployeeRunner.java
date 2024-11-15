7aa30ab22051cf61723f019c3fc63be5894c0df1
Demi Lune <anshikathakur596@gmail.com>
2024-11-15 12:17:26 +0530

    Update EmployeeRunner.java

--- object-oriented/src/oopslevel2/EmployeeRunner.java
+++ object-oriented/src/oopslevel2/EmployeeRunner.java
@@ -1,9 +1,22 @@
 package oopslevel2;
 
+import java.math.BigDecimal;
+
 public class EmployeeRunner {
     
         public static void main(String[] args) {
-            Employee employee = new Employee("Anshika", "anshikathakur147@gmail.com", "1234567897", "Software Engineer", "Google", "A", 1000000.00);
+            // Employee employee = new Employee("Anshika", "anshikathakur147@gmail.com", "1234567897", "Software Engineer", "Google", 'A',  new BigDecimal("75000.50"));
+            Employee employee = new Employee("Anshika", "anshikathakur147@gmail.com", "1234567897", "Software Engineer", "Google", 'A', new BigDecimal("75000.50"));
+            // to be implemented like this its not defined in a constructor
+            // employee.setName("Anshika");
+            // employee.setEmail("anshika@gmail.com");
+            // employee.setPhone("1234567890");
+            // employee.setTitle("Software Engineer");
+            // employee.setEmployer("Google");
+            // employee.setEmployeeGrade('A');
+            // employee.setSalary(new BigDecimal("75000.50"));
+            
+
             System.out.println(employee);
         }
 
