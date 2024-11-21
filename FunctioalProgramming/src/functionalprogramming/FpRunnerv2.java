ca562a750af404edca16ea484ccda74bf0bb37dc
Demi Lune <anshikathakur596@gmail.com>
2024-11-21 15:58:22 +0530

    Update FpRunnerv2.java

--- FunctioalProgramming/src/functionalprogramming/FpRunnerv2.java
+++ FunctioalProgramming/src/functionalprogramming/FpRunnerv2.java
@@ -4,12 +4,18 @@ import java.util.List;
 
 public class FpRunnerv2 {
     public static void main(String[] args) {
-        List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-        printFunctional(list);
+        List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+        printEvenNumbersInListFunctional(numbers);
+        numbers.stream()
+        .forEach(element -> System.out.println(element));
+        int sum = numbers.stream().reduce(0, (number1, number2) -> number1 + number2);
+        System.out.println("Sum is " + sum);
     }
-    private static void printFunctional(List<Integer> list) {
-        list.stream().forEach(element -> System.out.println("element - " + element));
 
-        list.stream().filter(element -> element % 2 == 0).forEach(element -> System.out.println("element - " + element));
+    private static void printEvenNumbersInListFunctional(List<Integer> numbers) {
+        numbers.stream()
+        .filter(number -> number % 2 == 0)
+        .forEach(element -> System.out.println(element));
     }
+
 }
